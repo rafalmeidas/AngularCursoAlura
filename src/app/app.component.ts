674c749229991998cd3e0dcf990986c4dc457d92
Rafael Almeida <supremotop10@gmail.com>
2020-07-29 20:40:17 -0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular';
+  photos = [
+    {
+      url: 'https://upload.wikimedia.org/wikipedia/commons/thumb/5/5a/Sultan_the_Barbary_Lion.jpg/440px-Sultan_the_Barbary_Lion.jpg',
+      description: 'Leão'
+    },
+    {
+      url: 'https://upload.wikimedia.org/wikipedia/commons/thumb/2/2d/Lioness_Etosha_NP.jpg/500px-Lioness_Etosha_NP.jpg',
+      description: 'Leoa'
+    },
+    {
+      url: 'https://thegamestower.files.wordpress.com/2011/01/capa-ac3a7c3a3o-games-ed-7.jpg',
+      description: 'Ação Games Clube'
+    },
+    {
+      url:"https://super.abril.com.br/wp-content/uploads/2018/07/capa_lol.jpg?quality=70&strip=info&w=720&h=930&crop=1",
+      description: "Revelations"
+    },
+    {
+      url:"https://www.selectgame.com.br/wp-content/uploads/2011/12/revista-oficial-playstation-edicao-157-capa-medio.jpg",
+      description:"League of Legends"
+    }
+  ];
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PhotosModule} from './photos/photos.module'
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    PhotosModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
